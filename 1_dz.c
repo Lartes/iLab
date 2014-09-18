e24b472dd4da3da920f99cc9c181d4c228228470
Lartes <artem-lomov@yandex.ru>
2014-09-19 00:04:09 +0400

    Replace some bugs

--- 1_dz.c
+++ 1_dz.c
@@ -1,34 +1,33 @@
 #include<stdio.h>
 #include<math.h>
 
-void kvad(double a,double b,double c,float *x1,float *x2, int *am_root){
+void solve_sqr(double a,double b,double c,float *x1,float *x2, int *root_amount){
 if (a==0){
         if (b==0){
             if (c==0){
-                *am_root=-1;  //An infinite number of roots
+                *root_amount=-1;  //An infinite number of roots
             }
             else
-                *am_root=0;  //No roots
+                *root_amount=0;  //No roots
         }
         else{
             *x1=-c/b;  //Linear equation - one root
-            *am_root=1;
+            *root_amount=1;
         }
     }
     else{
         double d=b*b-4*a*c; //Calculate the discriminant
-        printf("Discriminant D=%f\n",d);
         if (d<0){
-            *am_root=0;
+            *root_amount=0;
         }
         else{
             if (d==0){
-                *x1=-b/2/a; //Complete square - one root
-                *am_root=1;
+                *x1=-b/(2*a); //Complete square - one root
+                *root_amount=1;
             }
             else{
-                *x1=(-b+sqrt(d))/2/a; //First root
-                *x2=(-b-sqrt(d))/2/a; //Second root
+                *x1=(-b+sqrt(d))/(2*a); //First root
+                *x2=(-b-sqrt(d))/(2*a); //Second root
             }
         }
     }
@@ -37,16 +36,16 @@ if (a==0){
 
 int main(){
     double a=0,b=0,c=0;
-    int am_root=2;  //q=0 - no, 1 - one, 2 - two, -1 - any
+    int root_amount=2;  //root_amount = 0 - no, 1 - one, 2 - two, -1 - any
     float x1=0,x2=0,d=0;
 
     printf("Enter the coefficients of the quadratic equation ax^2+bx+c=0\n");
     scanf("%lg %lg %lg",&a,&b,&c);  //Input variables
 
-    kvad(a,b,c,&x1,&x2,&am_root);
+    solve_sqr(a,b,c,&x1,&x2,&root_amount);
 
-    switch(am_root){
-        case 0: printf("The equation has no roots");  //Output
+    switch(root_amount){
+        case 0: printf("The equation has no roots");  /* Output */
         break;
         case 1: printf("The equation has one root x=%f", x1);
         break;
