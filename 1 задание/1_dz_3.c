f209c1f218558b1b9c1339077c629a499bdbd30e
Lartes <artem-lomov@yandex.ru>
2014-09-28 00:21:33 +0400

    Third version
    The program is divided into three parts.
    Added check the validity of data.

---
+++ 1 задание/1_dz_3.c
@@ -0,0 +1,23 @@
+#include"solve_sqr.h"
+
+/**
+@mainpage Программа для решения квадратного уравнения
+*/
+
+int main(){
+    double a=0,b=0,c=0;
+    int root_amount=TWO_ROOTS;
+    double x1=0,x2=0,d=0;
+    char s='-';
+
+    printf("Enter the coefficients of the quadratic equation ax^2+bx+c=0\n");
+    scanf("%lg %lg %lg", &a, &b, &c);  //Input variables
+    scanf("%c",&s);
+    if (s!=10){       //Check the validity of data
+        printf("It is not a number");
+    } else{
+        solve_sqr(a,b,c,&x1,&x2,&root_amount);
+        out(x1,x2,root_amount);
+    }
+    return 0;
+}
