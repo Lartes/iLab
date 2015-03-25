1fa898652f4676560c670eaf5dac9d6bf61c1f16
Lartes <artem-lomov@yandex.ru>
2015-03-25 21:08:23 +0300

    Stack with template

---
+++ task_5_(stek) (tepmlate)/main.cpp
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#include"main.h"
+
+enum{
+    PUSH=1,
+    POP,
+    COUNT,
+    EXIT,
+};
+
+int main(){
+    Cstack<int> one;  //initialization is required for assert
+    int new_element, user_choise;
+    do{
+    printf("************\n");
+    printf("1 - Push\n");
+    printf("2 - Pop\n");
+    printf("3 - Get count\n");
+    printf("4 - Exit\n");
+    printf("************\n");
+    scanf("%d", &user_choise);
+    switch (user_choise){
+    case PUSH: printf("Enter element\n");
+            scanf("%d",&new_element);
+            one.push_stack(new_element);
+            break;
+    case POP: if (!one.isEmpty_stack()){
+                printf("_________________%d\n", one.pop_stack());
+                }
+            else{
+                printf("_________________Stack is empty\n");
+            }
+            break;
+    case COUNT: printf("______%d\n",one.getCount_stack());
+                break;
+    case EXIT: break;
+    };
+    } while (user_choise!=EXIT);
+    return 0;
+}
---
+++ task_5_(stek) (tepmlate)/main.h
@@ -0,0 +1,76 @@
+#include<stdio.h>
+#include<assert.h>
+#include<stdbool.h>
+
+#ifndef stackIlab
+#define stackIlab
+
+template <typename T>
+class Cstack{
+    public:
+        Cstack();
+        ~Cstack();
+        void push_stack(T element); //arguments: the address of pointer to the top item, element you want to add
+        T pop_stack(); //arguments: the address of pointer to the top item; result: element from item
+        bool isEmpty_stack(); //arguments: the address of the top item; result: true if empty, false if not empty
+        int getCount_stack(); //arguments: the address of the top item; result: count of elements
+    private:
+        struct item{
+            T value; //element
+            struct item* next; //pointer to the next item
+        };
+        struct item* begin_;
+        int count_;
+};
+
+template <typename T>
+Cstack<T>::Cstack(){
+    begin_=new struct item;
+    begin_->value=0;
+    begin_->next=NULL;
+    count_=0;
+}
+
+template <typename T>
+void Cstack<T>::push_stack(T element){
+    struct item *temp=begin_;
+    begin_=new struct item;
+    begin_->value=element;
+    begin_->next=temp;
+    count_++;
+}
+
+template <typename T>
+T Cstack<T>::pop_stack(){
+    assert((!isEmpty_stack()));  //when assert showed, you know what the error
+    T element=begin_->value;
+    struct item *temp=begin_;
+    begin_=begin_->next;
+    delete temp;
+    count_--;
+    return element;
+}
+
+template <typename T>
+Cstack<T>::~Cstack(){
+    struct item* temp=NULL;
+    while (begin_->next!=NULL){
+        temp=begin_;
+        begin_=begin_->next;
+        delete temp;
+    };
+    delete begin_;
+    begin_=NULL;
+}
+
+template <typename T>
+bool Cstack<T>::isEmpty_stack(){
+    if (begin_->next==NULL) return true; else return false;
+}
+
+template <typename T>
+int Cstack<T>::getCount_stack(){
+    return count_;
+}
+
+#endif
