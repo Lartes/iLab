3876466201f60b6600f3dd26d275263ee1066c64
Lartes <artem-lomov@yandex.ru>
2015-04-02 00:22:49 +0300

    Final version

--- task_6_(array)/array.h
+++ task_6_(array)/array.h
@@ -1,15 +1,20 @@
-#ifndef CArray
-#define CArray
+#ifndef CArrayh
+#define CArrayh
 
 #include<cassert>
 #include<iostream>
+#include<string.h>
 
 template <typename Data_T>
 class CArray{
 public:
     CArray(int size);
     ~CArray();
-    Data_t* at(int n);
+    Data_T* at(int n);
+    void add_mem(int how);
+    int fsize();
+    Data_T& operator[] (int n);
+    CArray<Data_T>& operator+ (CArray<Data_T>& a);
 private:
     CArray();
     Data_T* data_;
@@ -17,48 +22,78 @@ private:
 };
 
 template <typename Data_T>
-CArray <Data_t>::CArray(int size):
-    data_(new Data_t[size]),
-    size_(0)
+CArray <Data_T>::CArray(int size):
+    data_(new Data_T[size]),
+    size_(size)
     {
-    setmem(data_,0,sizeof(Data_t)*size);
+    memset(data_,0,sizeof(Data_T)*size);
     }
 
 
 template <typename Data_T>
-CArray <Data_t>::~CArray(){
+CArray <Data_T>::~CArray(){
     delete[] data_;
     data_=NULL;
     size_=0;
 }
 
 template <typename Data_T>
-Data_t* CArray <Data_t>::at(int n){
+Data_T* CArray <Data_T>::at(int n){
     assert(data_ != NULL);
     assert(n>=0 && n<size_);
     return &data_[n];
 }
 
-/*template <typename Data_T>
-void operator +(CArray<Data_T>& a, const CArray<Data_T>& b){ //нужно добавить поддержку ассициатисности: CArray Data_t&
+template <typename Data_T>
+void CArray <Data_T>::add_mem(int how){
+    int i;
+    Data_T* temp = new Data_T[size_+how+10];
+    for (i=0;i<size_;i++){
+        temp[i]=data_[i];
+    }
+    Data_T* temp2=data_;
+    data_=temp;
+    size_=size_+how+10;
+    free(temp2);
+}
 
-}*/
+template <typename Data_T>
+int CArray <Data_T>::fsize(){
+    return size_;
+}
 
-template<typename Data_T>
-Data_T& operator[] (int n){//& - ссылка
+template <typename Data_T>
+Data_T& CArray <Data_T>::operator[] (int n){//& - ссылка
     assert(data_ != NULL);
     assert(n>=0 && n<size_);
     return data_[n];
 }
 
+template <typename Data_T>
+CArray<Data_T>& CArray <Data_T>::operator+ (CArray<Data_T>& a){
+    int i, flag=0;
+    if (size_>a.fsize()){
+        flag=1;
+    }
+    CArray* temp;
+    temp=new CArray<Data_T>(10);
+    if (flag){
+        for (i=0;i<a.fsize();i++){
+            (*temp)[i]=data_[i]+a[i];
+        }
+        for (i=a.fsize();i<size_;i++){
+            (*temp)[i]=data_[i];
+        }
+    }
+    else{
+        for (i=0;i<size_;i++){
+            (*temp)[i]=data_[i]+a[i];
+        }
+        for (i=size_;i<a.fsize();i++){
+            (*temp)[i]=a[i];
+        }
+    }
+    return *temp;
+}
+
 #endif
-
-
-
-
-
-
-
-
-
-
--- task_6_(array)/main.cpp
+++ task_6_(array)/main.cpp
@@ -1,9 +1,20 @@
 #include<iostream>
 #include"array.h"
 
+using namespace std;
+typedef int type;
+
 int main(){
-    CArray <int> mas (10);
-    mas[0]=1;
-    printf("%d",mas[0]);
+    CArray <type> masA (2);
+    CArray <type> masB (3);
+    CArray <type> masC (1);
+    masA[0]=1;
+    masA[1]=2;
+    masB[0]=3;
+    masB[2]=4;
+    masC=masA+masB;
+    cout<<"[0]="<<masC[0]<<" [1]="<<masC[1]<<" [2]="<<masC[2]<<endl;
+    masC=masB+masA;
+    cout<<"[0]="<<masC[0]<<" [1]="<<masC[1]<<" [2]="<<masC[2]<<endl;
     return 0;
 }
