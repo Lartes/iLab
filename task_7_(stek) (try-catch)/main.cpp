58983f401686bf761a1e759d56f55ad16617820c
Lartes <artem-lomov@yandex.ru>
2015-04-21 17:58:12 +0300

    Stack with exception

---
+++ task_7_(stek) (try-catch)/main.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#include"main.h"
+
+using namespace std;
+
+enum{
+    PUSH=1,
+    POP,
+    COUNT,
+    EXIT,
+};
+
+int main(){
+    Cstack<int> one;  //initialization is required for assert
+    int new_element, user_choise;
+    do{
+    cout<<"************\n";
+    cout<<"1 - Push\n";
+    cout<<"2 - Pop\n";
+    cout<<"3 - Get count\n";
+    cout<<"4 - Exit\n";
+    cout<<"************\n";
+    cin >> user_choise;
+    switch (user_choise){
+    case PUSH: printf("Enter element\n");
+            scanf("%d",&new_element);
+            one.push(new_element);
+            break;
+    case POP: cout<<"element="<<one.pop()<<endl;
+            break;
+    case COUNT: cout<<"count="<<one.getCount()<<endl;
+                break;
+    case EXIT: break;
+    };
+    } while (user_choise!=EXIT);
+    return 0;
+}
---
+++ task_7_(stek) (try-catch)/main.h
@@ -0,0 +1,127 @@
+#include<stdio.h>
+#include<assert.h>
+#include<stdbool.h>
+#include<string.h>
+
+#ifndef stackIlab
+#define stackIlab
+
+//#define throw(a,b) {myexc one((a),(b)); throw one;}
+
+class myexc{
+    public:
+        myexc();
+        myexc(char name[10], int error);
+        ~myexc();
+        void what();
+    private:
+        char name_[10];
+        int error_;
+};
+
+myexc::myexc(){}
+
+myexc::myexc(char name[10], int error){
+    strcpy(name_,name);
+    error_=error;
+}
+
+myexc::~myexc(){
+    strcpy(name_,"");
+    error_=-1;
+}
+
+void myexc::what(){
+    printf("Error in function %s: ",name_);
+    switch(error_){
+    case 1: printf("stack is empty\n"); break;
+    case 2: printf("memory didn't allocated\n"); break;
+    default: printf("nondefinite error\n"); break;
+    }
+}
+
+
+
+template <typename T>
+class Cstack{
+    public:
+        Cstack();
+        ~Cstack();
+        void push(T element); //arguments: the address of pointer to the top item, element you want to add
+        T pop(); //arguments: the address of pointer to the top item; result: element from item
+        bool isEmpty(); //arguments: the address of the top item; result: true if empty, false if not empty
+        int getCount(); //arguments: the address of the top item; result: count of elements
+    private:
+        struct item{
+            T value; //element
+            struct item* next; //pointer to the next item
+        };
+        struct item* begin_;
+        int count_;
+};
+
+template <typename T>
+Cstack<T>::Cstack(){
+    begin_=NULL;
+    count_=0;
+}
+
+template <typename T>
+void Cstack<T>::push(T element){
+    try{
+    struct item *temp=begin_;
+    begin_=new struct item;
+    if (begin_==NULL) {myexc one("push",2); throw one;}
+    begin_->value=element;
+    if (temp==NULL)
+        begin_->next=NULL;
+    else
+        begin_->next=temp;
+    count_++;
+    }
+    catch(myexc error){
+    error.what();
+    exit(0);
+    }
+    catch(...){};
+}
+
+template <typename T>
+T Cstack<T>::pop(){
+    try{
+    if (begin_==NULL) {myexc one("push",1); throw one;}  //when assert showed, you know what the error
+    T element=begin_->value;
+    struct item *temp=begin_;
+    begin_=begin_->next;
+    delete temp;
+    count_--;
+    return element;
+    }
+    catch(myexc error){
+    error.what();
+    exit(0);
+    }
+}
+
+template <typename T>
+Cstack<T>::~Cstack(){
+    struct item* temp=NULL;
+    while (begin_!=NULL){
+        temp=begin_;
+        begin_=begin_->next;
+        delete temp;
+    };
+}
+
+template <typename T>
+bool Cstack<T>::isEmpty(){
+    if (begin_==NULL) return true;
+    return false;
+}
+
+template <typename T>
+int Cstack<T>::getCount(){
+    return count_;
+}
+
+#endif
